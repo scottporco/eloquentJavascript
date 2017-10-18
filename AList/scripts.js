4c7c5d5fcc36707e85477b2ce808ac6292e83bda
scottporco <scottporco@gmail.com>
2017-10-18 13:43:10 -0400

    A List Exercise

---
+++ AList/scripts.js
@@ -0,0 +1,28 @@
+var list = {
+    value : 1,
+    rest: {
+        value : 2,
+        rest : {
+            value : 3,
+            rest : null
+        }
+    }
+}
+
+var jsonStr = JSON.stringify(list);
+
+console.log(list);
+
+document.getElementById('output').innerHTML = jsonStr;
+
+
+function arrayToList(){
+    var myObjList = {
+        value : 2,
+        rest : null
+    }
+    
+    
+    myObjList.push()
+    
+}
